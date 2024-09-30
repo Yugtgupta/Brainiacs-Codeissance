b202193063a18e25b04e206d4c32406b15ec767c
Aryan shinde <shindearyan179@gmail.com>
2024-09-30 20:22:09 +0530

    LockScreen

--- frontendLatest/app/components/LockScreen.js
+++ frontendLatest/app/components/LockScreen.js
@@ -9,13 +9,16 @@ function LockScreen() {
           <h1 className="title">Welcome Back!</h1>
           <p className="subtitle">Please select your role to sign in</p>
         </div>
-
         <div className="role-buttons">
           <Link to="/register" className="role-btn mentor-btn">
             Sign in as Mentor
           </Link>
-          <button className="role-btn student-btn">Sign in as Student</button>
-          <button className="role-btn tutor-btn">Sign in as Tutor</button>
+          <Link to="/register" className="role-btn student-btn">
+            Sign in as Student
+          </Link>
+          <Link to="/register" className="role-btn tutor-btn">
+            Sign in as Tutor
+          </Link>
         </div>
       </div>
     </div>
---
+++ frontendLatest/app/components/ScholarshipPortal.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ScholarshipPortal = () => {
+  return <div>ScholarshipPortal</div>;
+};
+
+export default ScholarshipPortal;
