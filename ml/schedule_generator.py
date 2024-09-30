f2670f46891c4c7dd14a20dfddb8bbe737a50b33
Yugtgupta <yugtgupta@gmail.com>
2024-10-01 05:01:49 +0530

    roadmap generator,schedule planner and super_agent completed

--- ml/schedule_generator.py
+++ ml/schedule_generator.py
@@ -7,29 +7,33 @@ load_dotenv()
 import os
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-llm = ChatOpenAI(temperature=0.2,api_key=OPENAI_API_KEY)
-chat_history = []
-
-prompt = ChatPromptTemplate.from_messages([
-    ("system", """
-       You are a chatbot that helps me plan my schedule. You can ask questions to understand my requirements and suggest the best schedule for me to follow. 
-     Incorporate my daily activities, and personal time and put in time to study and work on projects. 
-     Be clear and concise in your responses as to what I should do in every two to three hours.
-     Ask me enough questions to understand my requirements properly before suggesting a schedule.
-     Make sure that the respoonse is personalized to my requirements.   
-    """),
-    MessagesPlaceholder(variable_name="chat_history"),
-    ("human","{input}")
-])
-
-chain = prompt|llm
-
-while(True):
-    question = input("You: ")
-    if question.lower() == "exit":
-        break
+def generate_schedule(question):
     
+    llm = ChatOpenAI(temperature=0.2,api_key=OPENAI_API_KEY)
+    chat_history = []
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", """
+        You are a chatbot that helps me plan my schedule. Suggest the best schedule for me to follow. 
+        Incorporate my daily activities, and personal time and put in time to study and work on projects. 
+        Be clear and concise in your responses as to what I should do in every two to three hours.  
+        """),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human","{input}")
+    ])
+
+    chain = prompt|llm
     response = chain.invoke({"input":question ,"chat_history":chat_history})
     chat_history.append(HumanMessage(question))
     chat_history.append(AIMessage(response.content))
-    print("Chatbot: ",response.content)
+    return response.content
+
+# while(True):
+#     question = input("You: ")
+#     if question.lower() == "exit":
+#         break
+    
+#     response = chain.invoke({"input":question ,"chat_history":chat_history})
+#     chat_history.append(HumanMessage(question))
+#     chat_history.append(AIMessage(response.content))
+#     print("Chatbot: ",response.content)
