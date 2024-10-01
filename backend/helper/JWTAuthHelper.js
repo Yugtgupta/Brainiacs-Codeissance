9f021b418f7d92dcd875d5885163c7edef4980da
Aryan shinde <shindearyan179@gmail.com>
2024-10-01 09:18:06 +0530

    Formatting

--- backend/helper/JWTAuthHelper.js
+++ backend/helper/JWTAuthHelper.js
@@ -1,9 +1,8 @@
-        
 const jwt = require("jsonwebtoken");
-const JsonResponse = require('./JsonResponse');
+const JsonResponse = require("./JsonResponse");
 
 exports.verifyToken = function (req, res, next) {
-  try{
+  try {
     const bearerToken = req.headers["authorization"];
 
     const bearer = bearerToken.split(" ");
@@ -13,19 +12,18 @@ exports.verifyToken = function (req, res, next) {
     console.log(token);
 
     req.apiUser = jwt.verify(token, process.env.JWTSECRET);
-    console.log(req.apiUser)  
-    next()
-  } catch(error){
-console.log("here")
+    console.log(req.apiUser);
+    next();
+  } catch (error) {
+    console.log("here");
     res.locals.data = {
-        isVaild: false, 
-        authorizationFailed: true,
-      };
+      isVaild: false,
+      authorizationFailed: true,
+    };
 
-      res.locals.message = error;
-      new JsonResponse(req, res).jsonError();
-      // next(error)
-      // return true
+    res.locals.message = error;
+    new JsonResponse(req, res).jsonError();
+    // next(error)
+    // return true
   }
 };
-        
